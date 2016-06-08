4ab16f798c637d51aac3a0e9d6e129a1b38734c3
KevinMoatsart <moatsart13@live.com>
2016-06-07 21:50:45 -0400

    first update

---
+++ MoatsFamily/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoatsFamily
+{
+    class Person
+    {
+
+        //enums
+        public enum Role
+        {
+            father,
+            mother,
+            son,
+            daughter
+        }
+
+
+        //data values
+        public string Name { get; set; }
+        public string Hobby { get; set; }
+        public int Age { get; set; }
+        public Role FamilyRole { get; set; }
+
+        //constructors
+        public Person() { }
+        public Person(string name, string hobby, int age, Role familyRole)
+        {
+            Name = name;
+            Hobby = hobby;
+            Age = age;
+            FamilyRole = familyRole;
+        }
+
+        //methods
+        public static void DisplayFamily(Person[] family)
+        {
+            foreach (Person person in family)
+            {
+                Console.WriteLine("{0} is {1}, their hobby is {2} and they are a {3} in the family.", person.Name, person.Age, person.Hobby, person.FamilyRole);
+            }
+        }
+
+    }
+}
---
+++ MoatsFamily/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoatsFamily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int familySize = 0;
+
+            Console.WriteLine("Please enter the size of your family.");
+            familySize = Int32.Parse(Console.ReadLine());
+            Person[] moatsFamily = new Person[familySize];
+
+            for (int i = 0; i < familySize; i++)
+            {
+                string name;
+                string hobby;
+                int age;
+                string testRole;
+                Person.Role role = Person.Role.son;
+
+                Console.WriteLine("Please enter family member {0}'s name", i + 1);
+                name = Console.ReadLine();
+                Console.WriteLine("Please enter family member {0}'s hobby", i + 1);
+                hobby = Console.ReadLine();
+                Console.WriteLine("Please enter family member {0}'s age", i + 1);
+                age = (Int32.Parse(Console.ReadLine()));
+                Console.WriteLine("Please enter family member {0}'s role (ex. father, mother, son, daughter)", i + 1);
+                testRole = Console.ReadLine();
+                try
+                    {
+                         role = (Person.Role)Enum.Parse(typeof(Person.Role), testRole);   
+                    }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine("You did not not enter a valid family role: \n\n{0}", e);
+                    Console.WriteLine("\n\nPlease copy and paste this code into an email to woops@.woops so we can fix it" +
+                       "in the future.\n\n");
+                }
+
+                moatsFamily[i] = new Person(name, hobby, age, role);
+             }
+
+            Person.DisplayFamily(moatsFamily);
+            Console.ReadLine();
+         }
+        }
+    }
